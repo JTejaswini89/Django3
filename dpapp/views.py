8b58fadb0c6853b0b28a596641b987d4d1c674ad
Tejaswini <tejaswinijonnagaddala@gmail.com>
2025-01-07 18:08:00 +0530

    Django2

---
+++ dpapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def dpfunction(request):
+    return render(request,'dp.html')
+# Create your views here.
+
+def virat(request):
+    return render(request,'virat.html')
+
+def rohit(request):
+    return render(request,'rohit.html')
+
+def rahul(request):
+    return render(request,'klrahul.html')
